c0c6cc237e73c0c83acc61b6662db622d89ebb03
Takashi Sogabe <takashi.sogabe@gmail.com>
2016-04-06 10:52:20 +0900

    step 3

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,10 +5,10 @@ BUNDLE_ROOT=/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYME
 yum install -y git
 
 # clone submodules
+rm -rf /tmp/WordPress
 cd /tmp
 git clone --recursive https://github.com/sogabe/WordPress.git
 cp -R /tmp/WordPress/bootstrap/* $BUNDLE_ROOT/bootstrap/
-rm -rf /tmp/WordPress
 
 yum groupinstall -y "Web Server" "MySQL Database" "PHP Support"
 yum install -y php-mysql
